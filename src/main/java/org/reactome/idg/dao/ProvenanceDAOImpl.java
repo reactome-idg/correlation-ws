5e860f2005687fd01194330cf6ba77cb7995a686
SolomonShorser-OICR <solomon.shorser@oicr.on.ca>
2019-06-07 17:57:49 -0400

    Add @Repository

--- src/main/java/org/reactome/idg/dao/ProvenanceDAOImpl.java
+++ src/main/java/org/reactome/idg/dao/ProvenanceDAOImpl.java
@@ -8,8 +8,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.reactome.idg.model.Provenance;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public class ProvenanceDAOImpl implements ProvenanceDAO
 {
 
